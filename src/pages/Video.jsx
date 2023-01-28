21e41b3c68bf608204171aea7ac4a08bdbd00afd
Anwar Hossain <mahedianwar@gmail.com>
2023-01-28 10:23:43 +0600

    Completed

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -114,9 +114,8 @@ const Video = () => {
             height="315"
             src="https://www.youtube.com/embed/5LbpwtzPhIk"
             title="YouTube video player"
-            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
+            allowFullScreen
           />
         </VideoWrapper>
         <Title>Test Video</Title>
